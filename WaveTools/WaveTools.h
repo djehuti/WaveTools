5fd1326a02d5af8821b2a0380ab196a04975880a
Ben Cox <cox@djehuti.com>
2012-07-17 22:11:32 -0400

    Add chunks to header

--- WaveTools/WaveTools.h
+++ WaveTools/WaveTools.h
@@ -9,3 +9,10 @@
 
 #import <WaveTools/WaveToolsBundleUtils.h>
 #import <WaveTools/DWTMath.h>
+#import <WaveTools/DWTWaveChunk.h>
+#import <WaveTools/DWTWaveUnknownChunk.h>
+#import <WaveTools/DWTWaveDataChunk.h>
+#import <WaveTools/DWTWaveFmtChunk.h>
+#import <WaveTools/DWTWaveGenericListChunk.h>
+#import <WaveTools/DWTWaveListInfoChunk.h>
+#import <WaveTools/DWTWaveStringChunk.h>
